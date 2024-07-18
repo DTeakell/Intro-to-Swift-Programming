71d9dc58140f615dfe836381e37f869d060db126
Dillon Teakell <teakelld@icloud.com>
2024-07-18 10:45:12 -0400

    Added Functions

--- Exercises/11 - Functions/Exercise-11.playground/Contents.swift
+++ Exercises/11 - Functions/Exercise-11.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
- 
+ var goldBars = 0
+
+// Declare function
+func unlockChest(goldInInventory: Int) -> Int {
+    var goldToOpenChest: Int = 4
+    let remainingGoldBars = goldBars - goldToOpenChest
+    
+    if remainingGoldBars < 0 {
+        print("You can't unlock the chest!")
+    } else {
+        print("Grab your loot!")
+        goldBars += 8
+    }
+    
+    return goldBars
+}
+
+// Call function
+unlockChest(goldInInventory: goldBars)
+
+print("You have \(goldBars) gold bars!")
+
