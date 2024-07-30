29965a708a4e6ff8c662903b9c54504d16f90117
Dillon Teakell <teakelld@icloud.com>
2024-07-29 21:54:53 -0500

    Redid Struct file

--- Exercises/14 - Structs/Exercise-14.playground/Contents.swift
+++ Exercises/14 - Structs/Exercise-14.playground/Contents.swift
@@ -1,37 +1,33 @@
 import UIKit
 
-struct TableReservation {
-    var name: String
-    let tableNumber: Int
-    let numberOfPeople: Int
+// Structs
+struct User {
+    let name: String
+    var jobTitle: String
+    var age: Int
+    let email: String
     
-    init(name: String, tableNumber: Int, numberOfPeople: Int) {
+    init(name: String, jobTitle: String, age: Int, email: String) {
         self.name = name
-        self.tableNumber = tableNumber
-        self.numberOfPeople = numberOfPeople
+        self.jobTitle = jobTitle
+        self.age = age
+        self.email = email
     }
     
-    // Print information about table
-    func printInformation() -> String {
-        return "Party Information \nName: \(name) \nTable Number: \(tableNumber) \nNumber of People: \(numberOfPeople)"
+    func printInformation() {
+        print("Name: \(name)")
+        print("Occupation: \(jobTitle)")
+        print("Age: \(age)")
+        print("Email: \(email)\n")
     }
     
-    // Update party information
-    mutating func updateBooking(updatedName: String) {
-        name = updatedName
-        print("\nName has been changed to \(name).\n")
+    mutating func isBirthdayToday() {
+        age += 1
     }
 }
 
-var reservation1 = TableReservation(
-    name: "Dillon",
-    tableNumber: 1,
-    numberOfPeople: 2
-)
+var user1 = User(name: "User", jobTitle: "iOS Developer", age: 22, email: "randomEmail@icloud.com")
+user1.printInformation()
 
-print(reservation1.printInformation())
-
-reservation1.updateBooking(updatedName: "Mya")
-print(reservation1.printInformation())
-
-
+user1.isBirthdayToday()
+user1.printInformation()
